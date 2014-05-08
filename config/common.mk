76f11c75d0d4233b3eaa68df51eb32dbb7bcf405
errantzebra845 <errantzebra845@gmail.com>
2014-05-08 14:10:52 -0400

    derp

--- config/common.mk
+++ config/common.mk
@@ -27,7 +27,7 @@ PRODUCT_COPY_FILES += \
 
 # CandyKat-specific init file
 PRODUCT_COPY_FILES += \
-    vendor/candykat/prebuilt/common/etc/init.local.rc:root/init.candykat.rc
+    vendor/candykat/prebuilt/common/etc/init.local.rc:root/init.slim.rc
 
 # Copy latinime for gesture typing
 PRODUCT_COPY_FILES += \

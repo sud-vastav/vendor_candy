b82222bb05c5a484cf76e512e553789027c7a689
mar5hal <errantzebra845@gmail.com>
2014-05-16 11:43:44 -0400

    add ville as a candy device
    remove jflte until we can get our hands on one

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,7 +2,6 @@ add_lunch_combo candykat_amami-userdebug
 add_lunch_combo candykat_flo-userdebug
 add_lunch_combo candykat_grouper-userdebug
 add_lunch_combo candykat_hammerhead-userdebug
-add_lunch_combo candykat_jflte-userdebug
 add_lunch_combo candykat_m7-userdebug
 add_lunch_combo candykat_m7spr-userdebug
 add_lunch_combo candykat_m7vzw-userdebug
@@ -10,4 +9,5 @@ add_lunch_combo candykat_maguro-userdebug
 add_lunch_combo candykat_mako-userdebug
 add_lunch_combo candykat_toro-userdebug
 add_lunch_combo candykat_toroplus-userdebug
+add_lunch_combo candykat_ville-userdebug
 

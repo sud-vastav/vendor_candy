a751f9e5062550ce390c860f281181a31f4aabbd
errantzebra845 <errantzebra845@gmail.com>
2014-05-20 16:18:08 -0400

    update candy version to go with the old style.

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,7 @@ add_lunch_combo candykat_hammerhead-userdebug
 add_lunch_combo candykat_m7-userdebug
 add_lunch_combo candykat_m7spr-userdebug
 add_lunch_combo candykat_m7vzw-userdebug
+add_lunch_combo candykat_m8-userdebug
 add_lunch_combo candykat_maguro-userdebug
 add_lunch_combo candykat_mako-userdebug
 add_lunch_combo candykat_toro-userdebug

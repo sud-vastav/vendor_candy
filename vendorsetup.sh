25da42f53342b60ca715095a425ca020708d4934
Ethan Johnson <cannondalev2000@gmail.com>
2014-06-27 23:48:09 -0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,6 +2,7 @@ add_lunch_combo candykat_amami-userdebug
 add_lunch_combo candykat_d2lte-userdebug
 add_lunch_combo candykat_evita-userdebug
 add_lunch_combo candykat_flo-userdebug
+add_lunch_combo candykat_flo-userdebug
 add_lunch_combo candykat_grouper-userdebug
 add_lunch_combo candykat_hammerhead-userdebug
 add_lunch_combo candykat_jflte-userdebug

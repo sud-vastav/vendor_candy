2e78e6828ba695e0f0ad97dce5e735f139e9cd59
gimmeitorilltell <gimmeitorilltell@yahoo.com>
2014-07-23 20:03:20 -0400

    add a few more devices to vendor

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -7,6 +7,7 @@ add_lunch_combo candykat_falcon-userdebug
 add_lunch_combo candykat_flo-userdebug
 add_lunch_combo candykat_grouper-userdebug
 add_lunch_combo candykat_hammerhead-userdebug
+add_lunch_combo candykat_i9300-userdebug
 add_lunch_combo candykat_jflte-userdebug
 add_lunch_combo candykat_m7-userdebug
 add_lunch_combo candykat_m7spr-userdebug
@@ -14,6 +15,8 @@ add_lunch_combo candykat_m7vzw-userdebug
 add_lunch_combo candykat_m8-userdebug
 add_lunch_combo candykat_maguro-userdebug
 add_lunch_combo candykat_mako-userdebug
+add_lunch_combo candykat_quincyatt-userdebug
+add_lunch_combo candykat_t6spr-userdebug
 add_lunch_combo candykat_toro-userdebug
 add_lunch_combo candykat_toroplus-userdebug
 add_lunch_combo candykat_ville-userdebug

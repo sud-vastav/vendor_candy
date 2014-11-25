c78981864c118ee2d606600f8af165d7faa5a9d1
rc420head <dennislebhardt@yahoo.com>
2014-11-24 23:59:08 -0600

    shut ethan up!
    Change-Id: If266d2c7f1bdc6e505f7e8fd07eddb90ef5c3d58

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,6 +2,9 @@ add_lunch_combo candy5_evita-userdebug
 add_lunch_combo candy5_falcon-userdebug
 add_lunch_combo candy5_grouper-userdebug
 add_lunch_combo candy5_hammerhead-userdebug
+add_lunch_combo candy5_m7-userdebug
+add_lunch_combo candy5_m7spr-userdebug
+add_lunch_combo candy5_m7vzw-userdebug
 add_lunch_combo candy5_m8-userdebug
 add_lunch_combo candy5_ville-userdebug
 

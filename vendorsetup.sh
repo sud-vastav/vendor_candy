ea3850f939a006945b1c24a27e19d37599a5d12f
rc420head <dennislebhardt@yahoo.com>
2014-11-24 19:35:49 -0600

    add ville
    Change-Id: I0da0118e0f62af64d1a189fa9e4f4ab11a1c8bb0

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,4 +4,5 @@ add_lunch_combo candy5_hammerhead-userdebug
 add_lunch_combo candy5_m7-userdebug
 add_lunch_combo candy5_m8-userdebug
 add_lunch_combo candy5_mako-userdebug
+add_lunch_combo candy5_ville-userdebug
 

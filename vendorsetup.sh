0ba82f7497d03a7030b4e2f9d1ac3470e5f26f40
Tim Klein <cuzz1369@gmail.com>
2014-12-07 14:00:27 -0500

    Update vendorsetup.sh
    Sorry Aaron

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,19 +1,22 @@
+add_lunch_combo candy5_d850-userdebug
+add_lunch_combo candy5_d851-userdebug
+add_lunch_combo candy5_d852-userdebug
+add_lunch_combo candy5_d855-userdebug
 add_lunch_combo candy5_evita-userdebug
 add_lunch_combo candy5_falcon-userdebug
+add_lunch_combo candy5_flo-userdebug
+add_lunch_combo candy5_flounder-userdebug
 add_lunch_combo candy5_grouper-userdebug
 add_lunch_combo candy5_hammerhead-userdebug
+add_lunch_combo candy5_ls990-userdebug
 add_lunch_combo candy5_m7-userdebug
 add_lunch_combo candy5_m7spr-userdebug
 add_lunch_combo candy5_m7vzw-userdebug
 add_lunch_combo candy5_m8-userdebug
 add_lunch_combo candy5_mako-userdebug
-add_lunch_combo candy5_d850-userdebug
-add_lunch_combo candy5_d851-userdebug
-add_lunch_combo candy5_d852-userdebug
-add_lunch_combo candy5_ls990-userdebug
-add_lunch_combo candy5_flo-userdebug
-add_lunch_combo candy5_d855-userdebug
-add_lunch_combo candy5_flounder-userdebug
+
+
+
 
 
 

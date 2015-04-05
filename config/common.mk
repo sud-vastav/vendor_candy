winder73/android_vendor_cyanide

SQUASH AdBlocker 3/3
Change-Id: I19df2d8617bd9d10833348da59c20eeb9ac816e8

Also updated hosts file from LiquidSmooth

config/common.mk
@@ -240,6 +240,11 @@ PRODUCT_PACKAGES += \
     procrank \
     Superuser \
     su
+    
+# HFM Files
+PRODUCT_COPY_FILES += \
+	vendor/cyanide/prebuilt/etc/hosts.alt:system/etc/hosts.alt \
+	vendor/cyanide/prebuilt/etc/hosts.og:system/etc/hosts.og
 
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.root_access=1

sud-vastav/vendor_candy

Conflicts:
	config/common.mk

config/common.mk
@@ -204,6 +204,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     OmniSwitch
 
+# HFM Files
+PRODUCT_COPY_FILES += \
+	vendor/candy5/prebuilt/etc/hosts.alt:system/etc/hosts.alt \
+	vendor/candy5/prebuilt/etc/hosts.og:system/etc/hosts.og
+
+
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.root_access=1
 else
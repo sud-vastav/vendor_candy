trader418/android_vendor_cm

Add latinime lib needed for Keyboard swyping

config/common.mk
@@ -113,6 +113,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/cm/prebuilt/common/etc/init.d/99fstrim:system/etc/init.d/99fstrim
 
+# Proprietary latinime lib needed for Keyboard swyping
+PRODUCT_COPY_FILES += \
+    vendor/cm/prebuilt/lib/libjni_latinimegoogle.so:system/lib/libjni_latinimegoogle.so
+
 # userinit support
 PRODUCT_COPY_FILES += \
     vendor/cm/prebuilt/common/etc/init.d/90userinit:system/etc/init.d/90userinit

sud-vastav/vendor_candy

config/common.mk
@@ -96,6 +96,10 @@ PRODUCT_COPY_FILES += \
     vendor/candy5/prebuilt/common/etc/init.d/00banner:system/etc/init.d/00banner \
     vendor/candy5/prebuilt/common/bin/sysinit:system/bin/sysinit
 
+# Proprietary latinime lib needed for Keyboard swyping
+PRODUCT_COPY_FILES += \
+    vendor/candy5/prebuilt/lib/libjni_latinimegoogle.so:system/lib/libjni_latinimegoogle.so
+
 # userinit support
 PRODUCT_COPY_FILES += \
     vendor/candy5/prebuilt/common/etc/init.d/90userinit:system/etc/init.d/90userinit
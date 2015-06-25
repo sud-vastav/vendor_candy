51cedc342fd33737057b269c4f68d4d13c148f06
Jalen Battle-Bryant <Flashalot.dev@gmail.com>
2015-06-25 15:32:24 -0400

    Add jewel

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -13,6 +13,7 @@ add_lunch_combo candy5_grouper-userdebug
 add_lunch_combo candy5_hammerhead-userdebug
 add_lunch_combo candy5_hercules-userdebug
 add_lunch_combo candy5_honami-userdebug
+add_lunch_combo candy5_jewel-userdebug
 add_lunch_combo candy5_lettuce-userdebug
 add_lunch_combo candy5_ls990-userdebug
 add_lunch_combo candy5_m7-userdebug
